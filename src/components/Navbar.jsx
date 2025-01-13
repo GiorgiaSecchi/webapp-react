21918a0520161c8911f8c8a6bff7d3a118b1c958
GiorgiaSecchi <secchigrg@gmail.com>
2025-01-13 15:00:11 +0100

    mod navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,9 +4,9 @@ export default function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
       <div className="container-fluid">
-        <a className="navbar-brand" to="#">
+        <NavLink className="navbar-brand" to="/">
           {import.meta.env.VITE_APP_NAME || "Web App"}
-        </a>
+        </NavLink>
         <button
           className="navbar-toggler"
           type="button"
@@ -20,9 +20,9 @@ export default function Navbar() {
         </button>
         <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
           <div className="navbar-nav">
-            <NavLink className="nav-link" aria-current="page" to="/">
+            {/* <NavLink className="nav-link" aria-current="page" to="/">
               Home
-            </NavLink>
+            </NavLink> */}
             <NavLink className="nav-link" to="/about">
               About
             </NavLink>
