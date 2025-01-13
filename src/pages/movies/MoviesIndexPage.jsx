d044f72f08d8ab1476529e993414c8e191b35c7b
GiorgiaSecchi <secchigrg@gmail.com>
2025-01-13 15:44:18 +0100

    test fetch MoviesIndexPage

--- src/pages/movies/MoviesIndexPage.jsx
+++ src/pages/movies/MoviesIndexPage.jsx
@@ -1,8 +1,24 @@
+import { useEffect, useState } from "react";
+
 export default function MoviesIndexPage() {
+  const [movies, seMovies] = useState([]);
+
+  useEffect(() => {
+    const url = "http://localhost:3000/api/movies";
+
+    fetch(url)
+      .then()
+      .then((res) => {
+        seMovies(res);
+      });
+  }, []);
+
   return (
     <>
       <div className="container py-5">
         <h1>Movies List</h1>
+
+        {movies}
       </div>
     </>
   );
