191023cf4c33a5fed496b9dfdb36d070d9158659
GiorgiaSecchi <secchigrg@gmail.com>
2025-01-14 18:45:45 +0100

    add generateStars

--- src/components/reviews/ReviewsItem.jsx
+++ src/components/reviews/ReviewsItem.jsx
@@ -1,11 +1,33 @@
 export default function ReviewsItem({ review }) {
+  // funzione per generare le stelle in base al dato di "vote"
+  const generateStars = (rating) => {
+    const stars = [];
+
+    for (let i = 0; i < 5; i++) {
+      //   console.log("i: ", i);
+      //   console.log("rating: ", rating);
+      //   console.log("i < rating: ", i < rating);
+      //   console.log("-------------");
+
+      const star = (
+        <i className={`fa-${i >= rating ? `regular` : `solid`} fa-star`}></i>
+      );
+      stars.push(star);
+    }
+    return stars;
+  };
+
   return (
     <div className="col-12 d-flex gap-3 align-items-center">
       <div>
         <div className="avatar">{review.name[0]}</div>
       </div>
       <div>{review.name}</div>
-      <div>{review.vote}</div>
+      <div>
+        {generateStars(review.vote).map((star, index) => (
+          <span key={index}>{star}</span>
+        ))}
+      </div>
       <div>{review.text}</div>
       <div>{review.created_at}</div>
     </div>
